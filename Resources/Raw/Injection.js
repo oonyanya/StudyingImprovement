b949d94ed05f5729981b9a515c2a7b554b07ed4e
rirufa <test@yahoo.co.jp>
2024-02-23 16:53:01 +0900

    置き換えるといろいろおかしくなるので、置き換えの部分を統合した

--- Resources/Raw/Injection.js
+++ Resources/Raw/Injection.js
@@ -37,22 +37,52 @@ function Inject_fn() {
         });
     }
 
+    function isHTML(htmlString) {
+      let parser = new DOMParser();
+      let doc = parser.parseFromString(htmlString, "application/xml");
+      let errorNode = doc.querySelector('parsererror');
+      return !errorNode;
+    }
+
+    function replace_element(element,  f)
+    {
+      element.childNodes.forEach((e)=>{
+        if(e.nodeType == Node.TEXT_NODE){
+          let new_text = f(e.textContent);
+          /* HTMLの文法としては正しくても成立しない可能性があるのでとりあえずつけておく */
+          if(isHTML("<html>" + new_text + "</html>")){
+            /* 当該ノードを置き換える */
+            let template = document.createElement('span');
+            template.innerHTML = new_text;
+            element.replaceChild(template, e);
+          }
+        }else if(e.nodeType == Node.ELEMENT_NODE){
+          e.innerHTML = f(e.innerHTML);
+        }
+      });
+    }
+
+    function wash_string_for_sreen_reader(s)
+   {
+      /* EDGEのスクリーンリーダーが途中で止まってしまう対策 */
+      return s.replaceAll( "・", "、");
+   }
+
     function add_folding_marker(elements) {
         const id = 0;
         elements.forEach((e) => {
-            /* 暗記ノート用マーカーがある場所はマーカーが機能しなくなるので無視する */
-            if (e.outerHTML.match("span-memory") == null) {
-                const new_html = e.innerHTML.replaceAll(/（/g, '<label class=\'folding_box\'>※<input type=\'checkbox\'></input><span>（').replaceAll(/）/g, '）</span></label>');
-                e.innerHTML = new_html;
-            }
+            replace_element(e, (s)=>{
+              let new_s =  s.replaceAll( /（/g, '<label class=\'folding_box\'>※<input type=\'checkbox\'></input><span>（').replaceAll( /）/g,  '）</span></label>');
+              return wash_string_for_sreen_reader(new_s);
+            });
         });
     }
 
-    function wash_text_for_screen_reader(elements) {
+    function wash_element_for_screen_reader(elements) {
         elements.forEach((e) => {
-             /* EDGEのスクリーンリーダーが途中で止まってしまう対策 */
-             const new_html = e.innerHTML.replaceAll("・","、");
-             e.innerHTML = new_html;
+             replace_element(e, (s)=>{
+                return wash_string_for_sreen_reader(s);
+             });
         });
     }
 
@@ -69,28 +99,24 @@ function Inject_fn() {
     add_temp_marker(elements, /[^ァ-ヴ][ァ-ヴ]　/);
 
     elements = document.querySelectorAll('#doc > h1');
-    wash_text_for_screen_reader(elements);
+    wash_element_for_screen_reader(elements);
 
     elements = document.querySelectorAll('#doc > h2');
-    wash_text_for_screen_reader(elements);
+    wash_element_for_screen_reader(elements);
 
     elements = document.querySelectorAll('#doc > h3');
-    wash_text_for_screen_reader(elements);
+    wash_element_for_screen_reader(elements);
 
     elements = document.querySelectorAll('#doc > p');
-    wash_text_for_screen_reader(elements);
     add_folding_marker(elements);
 
     elements = document.querySelectorAll('#doc > table > tbody > tr > td');
-    wash_text_for_screen_reader(elements);
     add_folding_marker(elements);
 
     elements = document.querySelectorAll('#doc > ul > li');
-    wash_text_for_screen_reader(elements);
     add_folding_marker(elements);
 
     elements = document.querySelectorAll('#doc > ol > li');
-    wash_text_for_screen_reader(elements);
     add_folding_marker(elements);
 
     elements = document.querySelectorAll('#answer_box_on > div > div > div > div > p');
