5cab61a53d45b296af7c26dc35b97b14f469e011
rirufa <test@yahoo.co.jp>
2024-03-10 16:43:56 +0900

    ウェブクライアントをカスタム実装するとJavaScriptが呼ばれないことがあった

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,7 +9,6 @@ namespace StudyingImprovement
         public MainPage()
         {
             InitializeComponent();
-            this.WebView.Navigated += WebView_Navigated;
             this.Loaded += MainPage_Loaded;
         }
 
@@ -51,29 +50,6 @@ namespace StudyingImprovement
         // Custom download listener for Android
         androidWebView.SetDownloadListener(new Platforms.Android.MyDownloadListener());
 #endif
-        }
-
-        private async void WebView_Navigated(object? sender, WebNavigatedEventArgs e)
-        {
-            System.Diagnostics.Debug.WriteLine(string.Format("navigating {0} {1}", e.Url, e.NavigationEvent.ToString()));
-            string css = await LoadAsset("Injection.css");
-            string js = await LoadAsset("Injection.js");
-            string inection_code = string.Format("function inject_css(){{var el=document.createElement('style');el.textContent = '{0}';document.head.append(el);}}if (document.readyState === 'complete'){{inject_css();}}else{{window.addEventListener('load', function(){{inject_css();}});}}{1}", css, js);
-            await this.WebView.EvaluateJavaScriptAsync(inection_code);
-        }
-
-        private async Task<string> LoadAsset(string name)
-        {
-            using var stream = await FileSystem.OpenAppPackageFileAsync(name);
-            using var reader = new StreamReader(stream);
-            StringBuilder sb = new StringBuilder();
-            string? line;
-            while ((line = await reader.ReadLineAsync()) != null)
-            {
-                sb.Append(line.Trim());
-            }
-            var contents = sb.ToString();
-            return contents;
         }
 
     }
