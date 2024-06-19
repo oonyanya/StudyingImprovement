436f825efa4af83132e056497a5cbead0f550d0a
rirufa <test@yahoo.co.jp>
2024-06-20 00:22:22 +0900

    WebView上でのシンタックスエラーを修正した

--- StudyingImprovementApp/Resources/Raw/Injection.js
+++ StudyingImprovementApp/Resources/Raw/Injection.js
@@ -94,10 +94,10 @@ function Inject_fn() {
     add_temp_marker(elements);
 
     elements = document.querySelectorAll('div.question_text > div > p');
-    add_temp_marker(elements, /[^ァ-ヴ][ァ-ヴ]　{3,}/); //「ア　ねこねこ」に一致
-    add_temp_marker(elements, /[1-9１-９]\s*((①|②|③|④|⑤|⑥|⑦|⑧|⑨)[ァ-ヴ]\s)+/); //「１ ①ア ①ア」に一致
-    add_temp_marker(elements, /[1-9１-９]\s*([Ａ-Ｚ][ァ-ヴ]\s*)+/); //「１ Ａウ ２ Ａウ」に一致
-    add_temp_marker_for_text(elements, /([1-9１-９]\s*[ァ-ヴ]+)/g); //「１ アウ ２ イウ」に一致
+    add_temp_marker(elements, /[^ァ-ヴ][ァ-ヴ]　{3,}/); /*「ア　ねこねこ」に一致 */
+    add_temp_marker(elements, /[1-9１-９]\s*((①|②|③|④|⑤|⑥|⑦|⑧|⑨)[ァ-ヴ]\s)+/); /*「１ ①ア ①ア」に一致 */
+    add_temp_marker(elements, /[1-9１-９]\s*([Ａ-Ｚ][ァ-ヴ]\s*)+/); /*「１ Ａウ ２ Ａウ」に一致 */
+    add_temp_marker_for_text(elements, /([1-9１-９]\s*[ァ-ヴ]+)/g); /*「１ アウ ２ イウ」に一致 */
     add_temp_marker_for_text(elements, /(.+)/g);
 
     add_temp_memo(document.querySelector("div.question_item"));
